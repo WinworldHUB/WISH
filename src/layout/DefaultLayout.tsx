aec796ed1eceb6141e7df933d7ea00a05a9dc16e
Amit Dubey <iamsaagar@yahoo.com>
2023-05-25 16:49:47 +0530

    Updated

---
+++ src/layout/DefaultLayout.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './Header';
+
+const DefaultLayout = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default DefaultLayout;
